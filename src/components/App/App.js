02f48cbbc5a4819d77dfc7c99fa3ca97eee0a83e
Immanuel512 <immanuelabiodun07@gmail.com>
2022-01-14 10:20:47 +0000

    components created

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Button from "../Button";
+import Quotes from "../Quotes";
+import Header from "../Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Quotes />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
