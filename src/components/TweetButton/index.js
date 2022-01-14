bcc7f4e96049340d0c1e6e7acd49a8fd26e2be79
tmserl <tom.earl@hey.com>
2022-01-14 15:55:32 +0000

    Small design tweaks

--- src/components/TweetButton/index.js
+++ src/components/TweetButton/index.js
@@ -1,11 +1,19 @@
+import "./TweetButton.css";
 import React from "react";
 
 function TweetButton({ handleTweet, fact }) {
-    console.log(fact.text)
+  console.log(fact.text);
   return (
-    <div>
-    <a href={`https://twitter.com/intent/tweet?text=${fact.text}`}
-    target="blank" onClick ={()=>{handleTweet()}}>Tweet</a>
+    <div className="tweet">
+      <a
+        href={`https://twitter.com/intent/tweet?text=${fact.text}`}
+        target="blank"
+        onClick={() => {
+          handleTweet();
+        }}
+      >
+        Tweet
+      </a>
     </div>
   );
 }
